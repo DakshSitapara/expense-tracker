1aadf6d5036185e699b44a77b2a5519f83a83847
DakshSitapara <dakshsitapara@gmail.com>
2025-06-19 17:55:05 +0530

    home page

--- app/(hybrid)/page.tsx
+++ app/(hybrid)/page.tsx
@@ -18,6 +18,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4 py-10 sm:px-8 md:px-16 lg:px-24">
       <div className="relative flex flex-col items-center w-full">
+                      <h1 className="text-4xl md:text-2xl  font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-gray-900 via-blue-800 to-gray-900 dark:from-white dark:via-blue-300 dark:to-white animate-gradient-x pb-2">
+            Track your Expense with <span className="font-bold" />
+        </h1>
         <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
           <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[180px] w-[180px] sm:h-[250px] sm:w-[250px] md:h-[310px] md:w-[310px] rounded-full bg-blue-500 opacity-20 blur-[80px] md:blur-[100px]"></div>
         </div>
@@ -38,9 +41,9 @@ export default function Home() {
         Register
           </Button>
         </Link>
-        <Link href="/expense-tracker" className="flex-1 w-full">
+        <Link href="https://github.com/DakshSitapara/expense-tracker" className="flex-1 w-full">
           <Button className="w-full bg-gray-900 rounded-xl py-6 text-lg font-semibold shadow-md transition hover:scale-105 hover:bg-gray-700">
-        Expense Tracker
+        Git Hub
           </Button>
         </Link>
       </div>
